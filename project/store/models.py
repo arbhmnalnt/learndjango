129ca18226ec7551396d2c5e8a5d797932815514
Ahmed Sayed <ahmedsayed124680@gmail.com>
2022-09-21 00:16:58 +0000

    Demo Creating a model

---
+++ project/store/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+
+class Product(models.Model):
+    name        = models.CharField(max_length=100)
+    stock_count = models.IntegerField()
+    price       = models.DecimalField(max_digits=6, decimal_places=2)
+
