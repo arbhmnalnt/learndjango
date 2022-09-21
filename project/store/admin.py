a9284ab220f58da7bb88143f71b435743e02aa40
Ahmed Sayed <ahmedsayed124680@gmail.com>
2022-09-21 17:06:09 +0000

    learn one to many relation and the development setting to upload media or images to the developement server in setting.py

--- project/store/admin.py
+++ project/store/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product, ProductImage
 
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('name', 'lastTimeupdated','createdTime')
+    list_display = ('name', 'lastTimeupdated','createdTime', 'created_at')
+
+class ProductImageAdmin(admin.ModelAdmin):
+    list_display = ('id', 'product','created_at','updated_at')
 # Register your models here.
 admin.site.register(Product, ProductAdmin)
+admin.site.register(ProductImage, ProductImageAdmin)
--- project/store/models.py
+++ project/store/models.py
@@ -4,8 +4,14 @@ from django.utils.timezone import now
 from django.db import models
 
 # Create your models here.
+class TimeStampMixin(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True,null=True)
+    updated_at = models.DateTimeField(auto_now=True,null=True)
 
-class Product(models.Model):
+    class Meta:
+        abstract = True
+
+class Product(TimeStampMixin,models.Model):
     name        = models.CharField(max_length=100)
     stock_count = models.IntegerField()
     price       = models.DecimalField(max_digits=6, decimal_places=2)
@@ -13,3 +19,9 @@ class Product(models.Model):
     department = models.CharField(max_length=100)
     createdTime = models.DateTimeField(auto_now_add=True, null=True,verbose_name = "time created")
     lastTimeupdated = models.DateTimeField(auto_now=True, null=True,verbose_name = "last time modified")
+
+class ProductImage(TimeStampMixin,models.Model):
+    image   = models.ImageField()
+    product = models.ForeignKey('Product', on_delete=models.CASCADE)
+    description = models.TextField(default="",blank=True)
+
