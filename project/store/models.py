866c13f8e0502f17c9dc220f6a35bfc28f08c1db
Ahmed Sayed <ahmedsayed124680@gmail.com>
2022-09-21 13:31:39 +0000

    django model fields and some options like [auto_now , aut_now_add,blanck, null] also the label to displayed for the column in admin interface , how to make some fields visible like created at and last modified and any other column in the admin page

--- project/store/models.py
+++ project/store/models.py
@@ -1,3 +1,6 @@
+import datetime
+from django.utils.timezone import now
+
 from django.db import models
 
 # Create your models here.
@@ -6,4 +9,7 @@ class Product(models.Model):
     name        = models.CharField(max_length=100)
     stock_count = models.IntegerField()
     price       = models.DecimalField(max_digits=6, decimal_places=2)
-
+    description = models.TextField(default="",blank=True)
+    department = models.CharField(max_length=100)
+    createdTime = models.DateTimeField(auto_now_add=True, null=True,verbose_name = "time created")
+    lastTimeupdated = models.DateTimeField(auto_now=True, null=True,verbose_name = "last time modified")
