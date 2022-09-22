b1177e6354b89a0588446e0c2101a2241fd7ae97
Ahmed Sayed <ahmedsayed124680@gmail.com>
2022-09-22 22:51:49 +0000

    simulating aswan green database model

--- project/store/admin.py
+++ project/store/admin.py
@@ -1,11 +1,55 @@
 from django.contrib import admin
-from .models import Product, ProductImage
+from .models import Departement, Employee, Attendance, Service, Client, Contract
 
-class ProductAdmin(admin.ModelAdmin):
-    list_display = ('name', 'lastTimeupdated','createdTime', 'created_at')
+class DepartementAdmin(admin.ModelAdmin):
+    list_display = ('name',)
 
-class ProductImageAdmin(admin.ModelAdmin):
-    list_display = ('id', 'product','created_at','updated_at')
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = ('eNum', 'name', 'departement','jobTitle','salaryType')
+
+class AttendanceAdmin(admin.ModelAdmin):
+    list_display = ('date','get_employees')
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        return qs.prefetch_related('employees')
+
+    def get_employees(self, obj):
+        return " - ".join([employee.name for employee in obj.employees.all()])
+
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = ('id','name','typee','priceType','supervisor','fixedDeliveryDate','fixedPriceCollectDate')
+
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ('id','name','phone','addressArea','created_at')
+
+class ContractAdmin(admin.ModelAdmin):
+    list_display = ('id','client','get_services','serialNum')
+    # optimizing the query for each service
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        return qs.prefetch_related('services')
+
+    def get_services(self, obj):
+        return " - ".join([service.name for service in obj.services.all()])
+
+
+
+
 # Register your models here.
-admin.site.register(Product, ProductAdmin)
-admin.site.register(ProductImage, ProductImageAdmin)
+admin.site.register(Departement, DepartementAdmin)
+admin.site.register(Employee, EmployeeAdmin)
+admin.site.register(Attendance, AttendanceAdmin)
+admin.site.register(Service, ServiceAdmin)
+admin.site.register(Client, ClientAdmin)
+admin.site.register(Contract, ContractAdmin)
+
+
+
+
+    # def get_services(self, obj):
+    #     return str("\n".join([str(p.services) for p in obj.services.all()]))
+
+    # def get_services(self, obj):
+        # services = [p.services for p in obj.services.all()]
+    #     services_names = [n.name for n in services]
+        # return " "+"\n".join([str(a.services) for a in obj.services.all()])
--- project/store/models.py
+++ project/store/models.py
@@ -11,17 +11,74 @@ class TimeStampMixin(models.Model):
     class Meta:
         abstract = True
 
-class Product(TimeStampMixin,models.Model):
-    name        = models.CharField(max_length=100)
-    stock_count = models.IntegerField()
-    price       = models.DecimalField(max_digits=6, decimal_places=2)
-    description = models.TextField(default="",blank=True)
-    department = models.CharField(max_length=100)
-    createdTime = models.DateTimeField(auto_now_add=True, null=True,verbose_name = "time created")
-    lastTimeupdated = models.DateTimeField(auto_now=True, null=True,verbose_name = "last time modified")
+    def __str__(self):
+        if hasattr(self, 'name'):
+            return self.name
+        elif hasattr(self, 'image'):
+            return str(self.id)
 
-class ProductImage(TimeStampMixin,models.Model):
-    image   = models.ImageField()
-    product = models.ForeignKey('Product', on_delete=models.CASCADE)
-    description = models.TextField(default="",blank=True)
+        elif hasattr(self, 'serialNum'):
+            return str(self.id)
+
+        elif hasattr(self, 'employees'):
+            return f"دفتر حضور يوم :  {self.date}"
+
+
+
+
+class Departement(TimeStampMixin,models.Model):
+    name  = models.CharField(max_length=50)
+    notes = models.TextField(max_length=50,null=True, blank=True)
+
+
+class Employee(TimeStampMixin,models.Model):
+    name         = models.CharField(max_length=50 , null=True, blank=True,  verbose_name="الاسم")
+    address      = models.CharField(max_length=50 , null=True, blank=True)
+    phone        = models.CharField(max_length=11, null=True, blank=True)
+    departement  = models.ForeignKey('Departement', on_delete=models.CASCADE,  null=True, blank=True)
+    jobTitle     = models.CharField(max_length=50, null=True, blank=True)
+    dateOfEmployment  = models.DateField(null=True, blank=True, verbose_name="تاريخ التعيين")
+    dateOfBirth  = models.DateField(null=True, blank=True)
+    naId         = models.CharField(max_length=14, null=True, blank=True)
+    typee        = models.CharField(max_length=50 , null=True, blank=True) # will be get front as [ employee or worker ]
+    salaryType   = models.CharField(max_length=50 , null=True, blank=True) # will be get front as [ daily or monthly ]
+    salary       = models.IntegerField(null=True, blank=True)
+    eNum         = models.IntegerField(null=True, blank=True, unique=True, verbose_name="الرقم التعريفى") # custom employee number for future us as like his id in company or any use else
+    notes        = models.TextField(max_length=50,null=True, blank=True)
+
+class Attendance(TimeStampMixin,models.Model):
+    date         = models.DateField(null=True, blank=True)
+    employees     = models.ManyToManyField('Employee',related_name='employees')
+    notes        = models.TextField(max_length=50,null=True, blank=True)
+
+
+class Service(TimeStampMixin,models.Model):
+    name                     = models.CharField(max_length=50,null=True, blank=True)
+    typee                    = models.CharField(max_length=50,null=True, blank=True)
+    price                    = models.IntegerField(null=True, blank=True)
+    priceType                = models.CharField(max_length=50,null=True, blank=True)
+    providers                = models.ManyToManyField('Employee',related_name='providers')
+    supervisor               = models.ForeignKey('Employee', related_name='supervisor', on_delete=models.CASCADE,null=True, blank=True)
+    fixedDeliveryDate        = models.IntegerField(default=1,null=True, blank=True) # check that the day nuumber is between 1-30 in api insert
+    fixedPriceCollectDate    = models.IntegerField(default=1,null=True, blank=True) # check that the day nnumber is between 1-30 in api insert
+    notes                    = models.TextField(max_length=50,null=True, blank=True)
+
+class Client(TimeStampMixin,models.Model):
+    name            = models.CharField(max_length=50,null=True, blank=True)
+    phone           = models.CharField(max_length=11, null=True, blank=True)
+    addressArea     = models.CharField(max_length=50,null=True, blank=True)
+    addressBuilding = models.CharField(max_length=50,null=True, blank=True, help_text="تفاصيل العمارة السكنية")
+    addressApartment= models.CharField(max_length=50,null=True, blank=True, help_text="تفاصيل الشقه")
+    addressDetails  = models.CharField(max_length=50,null=True, blank=True, help_text="اى تفاصيل إخرى للعنوان")
+    CNum            = models.IntegerField(null=True, blank=True, unique=True, verbose_name="الرقم التعريفى") # custom client number for future us as like his id in company or any use else
+    notes           = models.TextField(max_length=50,null=True, blank=True)
+
+class Contract(TimeStampMixin,models.Model):
+    serialNum       = models.IntegerField(help_text="رقم سريال متفرد لكل تعاقد",unique=True,null=True, blank=True)
+    client          = models.ForeignKey('Client', related_name='client', on_delete=models.CASCADE,null=True, blank=True)
+    services        = models.ManyToManyField('Service',related_name='services')
+    created_by      = models.ForeignKey('Employee', related_name='employee', on_delete=models.CASCADE,null=True, blank=True)
+    modified_by     = models.ForeignKey('Employee', on_delete=models.CASCADE,null=True, blank=True)
+    notes           = models.TextField(max_length=50,null=True, blank=True)
+
 
