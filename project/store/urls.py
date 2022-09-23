55bbac993401dde8c2c8881e4ff13a87c43015d4
Ahmed Sayed <ahmedsayed124680@gmail.com>
2022-09-23 11:47:46 +0000

    used the rest frame work and make the first api simple one

--- project/store/urls.py
+++ project/store/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('get_clients_api', views.get_clients_api.as_view(), name='get_client_api'),
+    path('get_client_services_api/<int:client_id>', views.get_client_services_api, name='get_client_services_api')
 ]
--- project/store/views.py
+++ project/store/views.py
@@ -1,6 +1,42 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404, get_list_or_404
+from django.http import HttpResponse,JsonResponse
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import *
+from django.core import serializers as core_serializers
+from .serializers import ContractSerializer, ServiceSerializer, ClientSerializer
 
 # Create your views here.
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+class get_clients_api(APIView):
+    def get(self, request):
+        clients = Client.objects.all()
+        data =  {'clients': list(clients.values("pk","name", ""))}
+        return JsonResponse(data)
+
+def get_client_services_api(request, client_id):
+    contract = get_object_or_404(Contract,client=client_id)
+    contractSerial = contract.serialNum
+    client = contract.client.name
+
+    servs=contract.services.all()
+    servss = [str(s.name) for s in servs]
+    # client = [str(x.name) for x in client]
+    # core_serializers.serialize('json', servs, fields=("name","typee"))
+    data =  {'contractSerial': contractSerial,'client':client, 'services':servss}
+
+    return JsonResponse(data)
+    # data =  ContractSerializer(Contract, many=True).data
+    # return Response(data)
+
+
+
+
+# class get_client_services_api(APIView):
+#     def get(self, request):
+#         clients = Client.objects.all()
+#         data =  {'clients': list(clients.values("pk","name", ""))}
+#         return JsonResponse(data)
+
